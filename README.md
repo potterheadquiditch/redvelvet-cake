0b7af85772d03d4c3eff029ecee79b9503507474
potterheadquiditch <74307161+potterheadquiditch@users.noreply.github.com>
2020-12-21 17:56:42 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# p5.play-boilerplate
+Boiler plate for p5.play
+tyuhfrb
+54dn86
+7rd6u7n
+e6758n
+r57b
+bry
+b5rb
+7bvfh
+tyu
+tfy
+bd
+ydt
+r67iuertuk
+mrs
+yy
+r7
+xer6
+i5677
+5
+tyo68t86tuk56kr568456romk7td
---
+++ sketch.js
@@ -0,0 +1,82 @@
+var img,image
+function preload(){
+
+}
+function setup() {
+  createCanvas(1000,1600);
+
+}
+
+function draw() {
+  background("redvelvet.jpg");  
+  textSize(48)
+  stroke("red")
+  text("RED VELVET CAKE RECIPE:-",10,50)
+
+  textSize(32)
+
+  text("Ingredients:-",15,90)
+
+  textSize(24)
+  text(" For the cake:",20,120)
+  text("2½  (310g) all-purpose flour",30,180)
+  text(" 2 tablespoons (16g) Cocoa powder ",30,210)
+  text("1 teaspoon Baking soda ",30,240)
+  text("1 teaspoon Salt ",30,270)
+  text("1½ cups (300g) Sugar ",30,300)
+  text("1 cup (240ml) buttermilk, room temperature",30,330)
+  text(" 1 cup – 1 tbsp (200g) Vegetable oil",30,360)
+  text(" 1 teaspoon white Vinegar ",30,390)
+  text("2 Eggs",30,420)
+  text(" 1/2 cup (115g) butter, room temperature ",30,450)
+  text("1-2 tablespoons Red food coloring",30,480)
+  text(" 2 teaspoons Vanilla extract",30,150)
+
+  textSize(24)
+  text(" For the frosting:",20,540)
+
+  text("1¼ cups (300ml) Heavy cream, cold ",30,570)
+  text("2 cups (450g) Cream cheese, room temperature ",30,600)
+  text("1½ cups (190g) Powdered sugar ",30,630)
+  text("1 teaspoon Vanilla extract",30,660)
+
+  textSize(32)
+  text(" Directions:-",15,720)
+
+  textSize(24)
+  text("1.Preheat oven to 350F (175C)",30,750)
+  text("2.In a large bowl sift flour, cocoa powder, baking soda and salt. ",30,800)
+  text("Stir and set aside.",30,830)
+  
+  text("3. In a separate large bowl, beat butter and sugar until smooth.",30,900)
+  text(" Add eggs one at the time, beating until combined after each addition",30,930)
+  text("Add oil, vinegar, vanilla extract, buttermilk and beat until combined",30,960)
+  text(" Gradually add the dry ingredients and mix after each addition",30,990)
+  text(" until well combined. Add food coloring and mix.",30,1020)
+
+  text("4. Pour the batter into two 8-inch (20cm) round baking pans",30,1100)
+  text("greased with butter and dust with flour. or you can use one pan.",30,1130)
+  text("and bake twice. Also you can use springrorm pan lined with parchment",30,1160)
+
+  text(" 5. Bake 35-40 minutes, until a toothpick inserted into the center comes out clean",30,1230)
+
+  text("6. Cool the cakes in their pans on a wire rack for 10 minutes. Release",30,1300)
+  text("from the pan, then let cool completely.",30,1330)
+
+  text("7. Make the frosting: in a large bowl, beat cream cheese with powdered sugar",30,1400)
+  text(" and vanilla extract. Beat until smooth and creamy. In a separate bowl ",30,1430)
+  text("with heavy cream to stiff peaks. Then fold into the cream cheese mixture.",30,1460)
+
+  text(" 8. Cut the top of the cakes. Cut 8-12 heart shapes from the ",30,1520)
+  text("top layer of the cakes. Make crumbs from the leftovers. Set aside.",30,1550)
+
+  text(" 9. Place one cake layer with flat side down. Spread a layer of frosting, ",30,1620)
+  text(" place the second layer of cake on top of the frosting, flat side up. ",30,1650)
+  text("Spread evenly the frosting on top and sides of the cake.",30,1680)
+
+  text("10. Decoration: Decorate the cake with the crumbs and the hearts. ",30,1750)
+
+  text("11. Refrigerate for at least 2-3 hours before serving",30,1820)
+
+  drawSprites();
+}
